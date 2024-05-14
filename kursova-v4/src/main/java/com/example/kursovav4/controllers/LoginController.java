4bf4169fcf9184cb379e7d3b6fda003935ca97d8
BlackCNP <mrx30050@gmail.com>
2024-05-14 22:51:42 +0300

    Alfa v0.0.4
    added html for login and register
    and controllers for sing in/up

---
+++ kursova-v4/src/main/java/com/example/kursovav4/controllers/LoginController.java
@@ -0,0 +1,14 @@
+package com.example.kursovav4.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+
+        return "login";
+    }
+}
