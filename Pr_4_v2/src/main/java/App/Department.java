8b2d9715552d49af18f9d658084b8c47a0892d9f
BlackCNP <mrx30050@gmail.com>
2024-05-14 15:04:13 +0300

    Add work

---
+++ Pr_4_v2/src/main/java/App/Department.java
@@ -0,0 +1,17 @@
+package App;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Department {
+    private Long id;
+    private String name;
+    private String title;
+    private String phone;
+    private String classroom;
+}
+
---
+++ Pr_4_v2/src/main/java/App/DepartmentController.java
@@ -0,0 +1,39 @@
+package App;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller
+@RequiredArgsConstructor
+public class DepartmentController {
+    private final DepartmentService departmentService;
+    @GetMapping("/departments")
+    public String departments(Model model) {
+        model.addAttribute("departments", departmentService.listDepartments());
+        return "departments";
+    }
+    @GetMapping("/departments/{id}")
+    public String departmentInfo(@PathVariable Long id, Model model) {
+        Department department = departmentService.getDepartmentById(id);
+        if (department != null) {
+            model.addAttribute("department", department);
+            return "department-info";
+        }
+        return "redirect:/departments";
+    }
+    @PostMapping("/departments/create")
+    public String createDepartment(Department department) {
+        departmentService.addDepartment(department);
+        return "redirect:/departments";
+    }
+    @PostMapping("/departments/delete/{id}")
+    public String deleteDepartment(@PathVariable Long id) {
+        departmentService.deleteDepartment(id);
+        return "redirect:/departments";
+    }
+}
