3c427116fc7585e7003b27ff01dc9c9ca266fee8
BlackCNP <mrx30050@gmail.com>
2024-05-15 14:53:16 +0300

    Added registration system and css fr login
    added a registration and entry system into the database and design for the login page

--- kursova-v4/src/main/java/com/example/kursovav4/controllers/RegisterController.java
+++ kursova-v4/src/main/java/com/example/kursovav4/controllers/RegisterController.java
@@ -5,6 +5,7 @@ import com.example.kursovav4.models.Account;
 import com.example.kursovav4.services.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +21,9 @@ public class RegisterController {
 
 
     @GetMapping("/register")
-    public String getRegisterPage() {
+    public String getRegisterPage(Model model) {
+        Account account = new Account();
+        model.addAttribute("account", account);
         return "register";
     }
 
