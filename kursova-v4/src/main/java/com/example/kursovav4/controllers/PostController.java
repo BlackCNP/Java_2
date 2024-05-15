8b23ed15a869495f14076e5aec819bb5b288808c
BlackCNP <mrx30050@gmail.com>
2024-05-15 19:52:01 +0300

    System of Edit Posts Beta 0.3

--- kursova-v4/src/main/java/com/example/kursovav4/controllers/PostController.java
+++ kursova-v4/src/main/java/com/example/kursovav4/controllers/PostController.java
@@ -5,8 +5,10 @@ import com.example.kursovav4.models.Post;
 import com.example.kursovav4.services.AccountService;
 import com.example.kursovav4.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,9 +28,9 @@ public class PostController {
     @GetMapping("/posts/{id}")
     public String getPost(@PathVariable Long id, Model model) {
 
-        // пошук     айді
+
         Optional<Post> optionalPost = this.postService.getById(id);
-        // якщо знайшов то сюди
+
         if (optionalPost.isPresent()) {
             Post post = optionalPost.get();
             model.addAttribute("post", post);
@@ -37,6 +39,25 @@ public class PostController {
             return "pomilka";
         }
     }
+
+
+    @PostMapping("/posts/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public String updatePost(@PathVariable Long id, Post post, BindingResult result, Model model) {
+
+        Optional<Post> optionalPost = postService.getById(id);
+        if (optionalPost.isPresent()) {
+            Post existingPost = optionalPost.get();
+
+            existingPost.setTitle(post.getTitle());
+            existingPost.setBody(post.getBody());
+
+            postService.save(existingPost);
+        }
+
+        return "redirect:/posts/" + post.getId();
+    }
+
 
     @GetMapping("/posts/create")
     public String createNewPost(Model model) {
@@ -57,5 +78,23 @@ public class PostController {
         postService.save(post);
         return "redirect:/posts/" + post.getId();
     }
+
+    @GetMapping("/posts/{id}/edit")
+    @PreAuthorize("isAuthenticated()")
+    public String getPostForEdit(@PathVariable Long id, Model model) {
+
+
+        Optional<Post> optionalPost = postService.getById(id);
+
+        if (optionalPost.isPresent()) {
+            Post post = optionalPost.get();
+            model.addAttribute("post", post);
+            return "post_edit";
+        } else {
+            return "pomilka";
+        }
+    }
+
+
 
 }
